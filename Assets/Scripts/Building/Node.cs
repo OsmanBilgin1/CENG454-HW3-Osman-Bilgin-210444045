ef4791be67d526a6ce65625cb45abe1f2b900785
OsmanBilgin1 <efman_su@hotmail.com>
2026-05-17 16:56:38 +0300

    feat: Building turret into nodes logic is implemented

--- Assets/Scripts/Building/Node.cs
+++ Assets/Scripts/Building/Node.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Node : MonoBehaviour 
 {
     public Color hoverColor;
+    public Vector3 positionOffset;
+    private GameObject turret;
     private Renderer rend;
     private Color startColor;
 
@@ -11,6 +13,17 @@ public class Node : MonoBehaviour
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
     }
+    void OnMouseDown ()
+	{
+		if (turret != null)
+		{
+			Debug.Log("Can't build there! - TODO: Display on screen.");
+			return;
+		}
+
+		GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+		turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+	}
 
     void OnMouseEnter()
     {
@@ -21,9 +34,6 @@ public class Node : MonoBehaviour
     {
         rend.material.color = startColor;
     }
-
-
-
 
 
 }
